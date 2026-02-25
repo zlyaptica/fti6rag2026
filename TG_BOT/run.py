6388fe0c616d0eb8e97610c51fdfc0fc3c2d64d8
zlyaptica <maximov021@mail.ru>
2026-02-25 21:23:25 +0300

    update tg bot project structure

--- TG_BOT/run.py
+++ TG_BOT/run.py
@@ -1,16 +1,34 @@
-import asyncio 
+import os
 import logging
-from config import settings
+import asyncio 
 from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
-from src.handlers.handlers import router
+from aiogram import F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, CallbackQuery
+from dotenv import load_dotenv
 
-bot = Bot(token=settings.tg_model_apikey)
+load_dotenv(".env.local")
+
+tg_token = os.getenv("TG_BOT_APIKEY")
+if not tg_token:
+    raise ValueError("Токен телеграм бота не найден")
+
 dp = Dispatcher(storage=MemoryStorage())
+
+@dp.message(CommandStart())
+async def handle_start_message(message: Message):
+    await message.answer(
+        f"Привет, {message.from_user.full_name}! Добро пожаловать в бота, который скоро все будет знать! Напиши, что ты хочешь узнать? Напишите запрос и бот напишет все, что знает."
+    )
+
+@dp.message(F.text)
+async def handle_question(message: Message):
+    await message.answer(message.text)
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
-    dp.include_router(router)
+    bot = Bot(token=tg_token)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
--- TG_BOT/src/utils/handlers_helpers.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import Message
-
-
-async def send_help_info(message: Message):
-    await message.answer("Я могу найти любую информацию")
